7ad7657c5e514b3fa1438cf3e956e587d48fde57
p-fisher <ops.patrickfisher@gmail.com>
2022-03-12 14:51:57 -0500

    gose

--- wk1_React/firstReact/create-react-app/src/App.js
+++ wk1_React/firstReact/create-react-app/src/App.js
@@ -1,22 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
+        <h1>Hello Dojo!</h1>
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+          <h3>Things I need to do:</h3>
+          <ul>
+            <li>Learn React</li>
+            <li>Climb Mt. Everest</li>
+            <li>Run a marathon</li>
+            <li>Feed the danj</li>
+          </ul>
+          {/* Edit <code>src/App.js</code> and save to reload. */}
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
       </header>
     </div>
   );
