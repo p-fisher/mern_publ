1438d87dfc39142acb675d4d638c4ee561a68e78
p-fisher <ops.patrickfisher@gmail.com>
2022-03-12 15:03:15 -0500

    gose

--- wk1_React/firstReact/create-react-app/src/App.test.js
+++ wk1_React/firstReact/create-react-app/src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders feed the danj', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const myText = screen.getByText('danj');
+  expect(myText).toBeInTheDocument();
 });
