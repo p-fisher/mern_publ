cd7dec910226b5409b9bc53d44ec0fcb839a4a2f
p-fisher <ops.patrickfisher@gmail.com>
2022-03-12 19:54:05 -0500

    goot

--- wk1_React/firstReact/prop-up/src/App.js
+++ wk1_React/firstReact/prop-up/src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+
+import Person from './components/PersonCard';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Person firstName={ "Mary" } lastName={ "Penrod" } age={ 44 } hairColor={ "silver-blue" } />
+      <Person firstName={ "Bill" } lastName={ "Smithers" } age={ 56 } hairColor={ "no hair" } />
+      <Person firstName={ "Chester" } lastName={ "Jackson" } age={ 32 } hairColor={ "black" } />
+      <Person firstName={ "Woody" } lastName={ "Klein" } age={ 71 } hairColor={ "gray" } />      
     </div>
   );
 }
-
 export default App;
