d21d8d8a6ce7ce1975328799741f1b04e923cacf
p-fisher <ops.patrickfisher@gmail.com>
2022-03-15 12:19:39 -0400

    moving

--- wk2_React/prop-up-2/src/App.js
+++ wk2_React/prop-up-2/src/App.js
@@ -2,13 +2,50 @@ import './App.css';
 
 import Person from './components/PersonCard';
 
+const personArray = [
+  {
+    firstName: "Mary",
+    lastName: "Penrod",
+    age: 44,
+    hairColor: "silver-blue"
+  },
+  {
+    firstName: "Bill",
+    lastName: "Smithers",
+    age: 56,
+    hairColor: "no hair"
+  },
+  {
+    firstName: "Chester",
+    lastName: "Jackson",
+    age: 32,
+    hairColor: "black"
+  },
+  {
+    firstName: "Woody",
+    lastName: "klein",
+    age: 71,
+    hairColor: "gray"
+  }
+];
+
 function App() {
   return (
     <div className="App">
-      <Person firstName={ "Mary" } lastName={ "Penrod" } age={ 44 } hairColor={ "silver-blue" } />
-      <Person firstName={ "Bill" } lastName={ "Smithers" } age={ 56 } hairColor={ "no hair" } />
-      <Person firstName={ "Chester" } lastName={ "Jackson" } age={ 32 } hairColor={ "black" } />
-      <Person firstName={ "Woody" } lastName={ "Klein" } age={ 71 } hairColor={ "gray" } />      
+      {personArray.map((personObject, index) => (
+        <Person 
+          key = {index}
+          firstName = {personObject.firstName}
+          lastName = {personObject.lastName}
+          age = {personObject.age}
+          hairColor = {personObject.hairColor} 
+        />
+        ))}
+
+      {/* <Person firstName={ personArray[0].firstName } lastName={ personArray[0].lastName } age={ personArray[0].age } hairColor={ personArray[0].hairColor } />
+      <Person firstName={ personArray[1].firstName } lastName={ personArray[1].lastName } age={ personArray[1].age } hairColor={ personArray[1].hairColor } />
+      <Person firstName={ personArray[2].firstName } lastName={ personArray[2].lastName } age={ personArray[2].age } hairColor={ personArray[2].hairColor } />
+      <Person firstName={ personArray[3].firstName } lastName={ personArray[3].lastName } age={ personArray[3].age } hairColor={ personArray[3].hairColor } /> */}
     </div>
   );
 }
--- wk2_React/prop-up-2/src/components/PersonCard.js
+++ wk2_React/prop-up-2/src/components/PersonCard.js
@@ -1,15 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
     
 const Person = (props) => {
 
     const { firstName, lastName, age, hairColor } = props;
+    const [stateOfAge, setStateOfAge] = useState(age);
     return (
         <div>
             <h1>{lastName}, {firstName}</h1>
-            <p>Age: {age}</p>
+            <p>Age: {stateOfAge}</p>
             <p>Hair Color: {hairColor}</p>
+            {/* <button onClick={() => setStateOfAge(stateOfAge+ 1)}>
+                Birthday Button: {firstName} {lastName}
+            </button> */}
+            <button onClick={() => setStateOfAge(stateOfAge+ 1)}>
+                Add 1 year to {firstName}'s Age!
+            </button>
+
         </div>
     );
 }
 export default Person;
-
