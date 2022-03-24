4dd270e56b9f77c8622ea982cd3b76332e590f83
p-fisher <ops.patrickfisher@gmail.com>
2022-03-24 14:06:16 -0400

    still chugging

--- wk3_React/box_generator/src/components/BoxDisplay.js
+++ wk3_React/box_generator/src/components/BoxDisplay.js
@@ -9,7 +9,10 @@ const Display = (props) => {
     //     setAllTheBoxes([...allTheBoxes,boxColor])
     // }
     return (
-        <div className="form_wrapper">
+
+// this is not yet working, so i have also done this functionality in the ./components/BoxForm.js until I figure how to life the state
+// note: import Form above is commented out and in App.js BoxDisplay (this file) is commented out
+<div className="form_wrapper">
             <h2>And here they are...</h2>
                 {allTheBoxes.map((color,index) => {
                     return (
