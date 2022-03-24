83dee16879cef2a0b4a042c8724b621c6e930f7d
p-fisher <ops.patrickfisher@gmail.com>
2022-03-24 15:12:45 -0400

    closing in

--- wk3_React/box_generator/src/components/BoxDisplay.js
+++ wk3_React/box_generator/src/components/BoxDisplay.js
@@ -2,24 +2,25 @@ import React, { useState } from "react";
 // import Form from "./BoxForm";
 
 const Display = (props) => {
-    // const [boxColor, setBoxColor] = useState("");
-    // const [allTheBoxes, setAllTheBoxes] = useState([]);
-    // const addBoxToBoxes = (e) => {
-    //     e.preventDefault();
-    //     setAllTheBoxes([...allTheBoxes,boxColor])
-    // }
     return (
-
-// this is not yet working, so i have also done this functionality in the ./components/BoxForm.js until I figure how to life the state
-// note: import Form above is commented out and in App.js BoxDisplay (this file) is commented out
-<div className="form_wrapper">
+        // this is not yet working, so i have also done this functionality in the ./components/BoxForm.js until I figure how to life the state
+        // note: import Form above is commented out and in App.js BoxDisplay (this file) is commented out
+        <div className="form_wrapper">
             <h2>And here they are...</h2>
-                {allTheBoxes.map((color,index) => {
-                    return (
-                    <div key={index} style={{ height:'100px', width:'100px', display: 'inline-flex', margin: '0 10px', backgroundColor: color }}>
-                    </div>
-                    )})}
-
+            {props.allTheBoxes.map((color, index) => {
+                return (
+                    <div
+                        key={index}
+                        style={{
+                            height: "100px",
+                            width: "100px",
+                            display: "inline-flex",
+                            margin: "0 10px",
+                            backgroundColor: color,
+                        }}
+                    ></div>
+                );
+            })}
         </div>
     );
 };
--- wk3_React/box_generator/src/components/BoxForm.js
+++ wk3_React/box_generator/src/components/BoxForm.js
@@ -1,12 +1,14 @@
 import React, { useState } from "react";
+import BoxDisplay from "./BoxDisplay";
 
 const Form = (props) => {
     const [boxColor, setBoxColor] = useState("");
     const [allTheBoxes, setAllTheBoxes] = useState([]);
     const addBoxToBoxes = (e) => {
         e.preventDefault();
-        setAllTheBoxes([...allTheBoxes,boxColor])
-    }
+        setAllTheBoxes([...allTheBoxes, boxColor]);
+        setBoxColor("");
+    };
     return (
         <div className="form_wrapper">
             <h2>Make some color boxes.</h2>
@@ -15,25 +17,22 @@ const Form = (props) => {
                 <div>
                     <label htmlFor="color">Name a color: </label>
                     <input
+                        value={boxColor}
                         type="text"
                         name="color"
                         onChange={(e) => setBoxColor(e.target.value)}
                     />
-                    <button type="submit" className="btn_submit">Add</button>
+                    <button type="submit" className="btn_submit">
+                        Add
+                    </button>
                 </div>
             </form>
             <div>
-                <h2>And here they be...</h2>
-                {allTheBoxes.map((color,index) => {
-                    return (
-                    <div key={index} style={{ height:'100px', width:'100px', display: 'inline-flex', margin: '0 10px', backgroundColor: color }}>
-                    </div>
-                    )})}
+                {/* <h2>And here they be...</h2> */}
+                <BoxDisplay allTheBoxes={allTheBoxes} />
             </div>
         </div>
     );
 };
-
-
 
 export default Form;
