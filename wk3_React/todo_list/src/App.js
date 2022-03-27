b306f17c6e570ea02c9bcf9ccec69b0193c7ef34
p-fisher <ops.patrickfisher@gmail.com>
2022-03-27 18:11:46 -0400

    progs

--- wk3_React/todo_list/src/App.js
+++ wk3_React/todo_list/src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
+// import /*some component*/ from './components/(the_component_filename)';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [newItem, setNewItem] = useState("");
+    const [items, setItems] = useState([]);
+
+    const submitEvent = (e) => {
+        e.preventDefault();
+        setItems([...items, newItem]);
+        setNewItem("");
+    };
+
+    const removeEvent = (killItem) => {
+        const filteredItems = items.filter((item, index) => {
+            return index !== killItem;
+        });
+        setItems(filteredItems);
+    };
+
+    return (
+        <div style={{ marginTop: "30px", textAlign: "center" }}>
+            <h2>Add some to-do items...</h2>
+            <form
+                onSubmit={(e) => {
+                    submitEvent(e);
+                }}
+            >
+                <input
+                    onChange={(e) => {
+                        setNewItem(e.target.value);
+                    }}
+                    type="text"
+                    value={newItem}
+                />
+                <div>
+                    <button id="button_add" type="submit">
+                        Add Item
+                    </button>
+                </div>
+            </form>
+
+            {/* ---------------------- DISPLAY LIST ------------------------------- */}
+            <div className="items_list">
+                <h2>Now, get crackin'!</h2>
+                {items.map((item, index) => {
+                    return (
+                        <div className="items_row" key={index}>
+                            <div className="items_desc" >
+                                {item}
+                            </div>
+                            <div className="items_chkbx">
+                                <input type="checkbox" />
+                            </div>
+                            <div className="items_del">
+                                <a
+                                    href="#"
+                                    onClick={(e) => {
+                                        removeEvent(index);
+                                    }}
+                                >
+                                    Delete
+                                </a>
+                            </div>
+                        </div>
+                    );
+                })}
+            </div>
+        </div>
+    );
 }
 
 export default App;
