39885f0986f5c95ae96e5879eb10fb1b115382f7
p-fisher <ops.patrickfisher@gmail.com>
2022-03-27 00:14:23 -0400

    fin

--- wk3_React/tabs_excs/src/components/Results.js
+++ wk3_React/tabs_excs/src/components/Results.js
@@ -1,11 +1,26 @@
 import React from 'react';
+// import Tabs from './components/Tabs';
 
 const Results = (props) => {
-    const { allTheTabs, currentTab } = props;
+    const { allTheTabs, currentTab, index } = props;
+
+
+    const resultsBgnd = (index) => {
+        if ( currentTab === 0 ) {
+                return "applesBgnd";
+            } else if (currentTab === 1 ) {
+                return "orangesBgnd"
+            } else if (currentTab === 2 ) {
+                return "bananasBgnd"
+            } else {
+                return "noBgnd";
+            }
+        }
+    
 
     return (
-        <div className="tabs_blurb">
-            { allTheTabs[currentTab].blurb }
+        <div className={`tabs_blurb ${ resultsBgnd(index)}`}>
+            <h2 style={{textShadow: "2px 2px 3px white"}}>{ allTheTabs[currentTab].blurb }</h2>
         </div>
     )
 }
