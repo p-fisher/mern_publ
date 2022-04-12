269062cf3fe408cbea234354114ebd6f4d3b5821
p-fisher <ops.patrickfisher@gmail.com>
2022-04-12 16:05:41 -0400

    fin?

--- wk6_full_stack/safe myNewProject/server/controllers/person.controller.js
+++
@@ -1,7 +0,0 @@
-module.exports.index = (request, response) => {
-    //We are exporting a key:val pair of index : function
-    response.json({
-        // This is where we're setting the API's response to the requesting client
-        message: "Hello World",
-    });
-};
--- wk6_full_stack/safe myNewProject/server/routes/person.routes.js
+++
@@ -1,4 +0,0 @@
-const PersonController = require("../controllers/person.controller"); //Import the code from Code Block 1
-module.exports = (app) => {
-    app.get("/api", PersonController.index);
-};
