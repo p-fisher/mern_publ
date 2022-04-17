f82b974c09faa92beee083c24f72d2a7be9437d5
p-fisher <ops.patrickfisher@gmail.com>
2022-04-17 12:50:56 -0400

    progressus

--- wk6_full_stack/product_manager_3/server/controllers/product.controller.js
+++ wk6_full_stack/product_manager_3/server/controllers/product.controller.js
@@ -2,7 +2,7 @@ const Product = require("../models/product.model.js");
 
 module.exports = {
     getAllProducts: (req, res) => {
-        Product.find({}) 
+        Product.find({})
             .then((allProducts) => res.json(allProducts))
             .catch((err) => console.log(err));
     },
@@ -12,10 +12,28 @@ module.exports = {
             .then((oneProduct) => res.json(oneProduct))
             .catch((err) => console.log(err));
     },
-    
+
     createProduct: (req, res) => {
         Product.create(req.body)
             .then((newProduct) => res.json(newProduct))
+            .catch((err) => console.log(err));
+    },
+
+    // refer to videos about this piece
+
+    // We must first find the document before we can add in the new info.
+    updateProduct: (req, res) => {
+        Product.findByIdAndUpdate({ _id: req.params.id }, req.body, {
+            new: true, //By default, update fill not return a new document. This is necessary to ensure we respond with the newly updated document.
+            runValidators: true, //This ensures validators work on a PUT request.
+        })
+            .then((updatedProduct) => res.json(updatedProduct))
+            .catch((err) => console.log(err));
+    },
+
+    deleteProduct: (req, res) => {
+        Product.deleteOne({ _id: req.params.id })
+            .then((deletedId) => res.json(deletedId))
             .catch((err) => console.log(err));
     },
 };
