ece68c00311d7ca49143d3914e1a3c63c5990da0
p-fisher <ops.patrickfisher@gmail.com>
2022-04-27 20:34:08 -0400

    wkg

---
+++ wk8_oop/SuperNinja/superninja.js
@@ -0,0 +1,66 @@
+// Extend the Ninja class and create the Sensei class.
+//    A Sensei should have 200 Health, 10 speed, and 10 strength by default.
+//    In addition, a Sensei should have a new attribute called wisdom, and the default should be 10.
+//    Finally, add the speakWisdom() method. speakWisdom() should call the drinkSake() method from the Ninja class, before console.logging a wise message.
+
+class Ninja {
+    constructor(name, health, speed = 3, strength = 3) {
+        this.name = name;
+        this.health = health;
+        this.speed = speed;
+        this.strength = strength;
+    }
+
+    sayName() {
+        console.log("Name is: " + this.name);
+    }
+
+    showStats() {
+        console.log("Stats: " + this.name);
+        console.log("Health: " + this.health);
+        console.log("Speed: " + this.speed);
+        console.log("Strength: " + this.strength);
+    }
+
+    drinkSake() {
+        this.health += 10;
+        console.log(
+            this.name + " consumed sake. Health is now: " + this.health
+        );
+    }
+}
+
+class Sensei extends Ninja {
+    constructor(name, health = 200, speed = 10, strength = 10, wisdom = 10) {
+        super(name, health, speed, strength);
+        this.wisdom = wisdom;
+    }
+
+    speakWisdom() {
+        console.log("Some wisdom...");
+        this.drinkSake();
+        console.log(
+            "Jack Handy indicates the crows seemed to be calling Kaw's name."
+        );
+    }
+}
+
+console.log("\n  Upcoming: Chuck");
+const ninja1 = new Ninja("Chuck", 10);
+ninja1.sayName();
+ninja1.showStats();
+ninja1.drinkSake();
+// console.log(ninja1);
+
+console.log("\n  Upcoming: Mary");
+const ninja2 = new Ninja("Mary", 20, 5, 7);
+ninja2.sayName();
+ninja2.showStats();
+ninja2.drinkSake();
+// console.log(ninja2);
+
+console.log("\n  Upcoming: Sensei Mike");
+const sensei1 = new Sensei("Sensei Mike");
+sensei1.showStats();
+sensei1.speakWisdom();
+// console.log(sensei1);
---
+++ wk8_oop/ninja/ninja.js
@@ -0,0 +1,40 @@
+class ninja {
+    constructor(name, health, speed = 3, strength = 3) {
+        this.name = name;
+        this.health = health;
+        this.speed = speed;
+        this.strength = strength;
+    }
+
+    sayName() {
+        console.log("Name is: " + this.name);
+    }
+
+    showStats() {
+        console.log("Stats: " + this.name);
+        console.log("Health: " + this.health);
+        console.log("Speed: " + this.speed);
+        console.log("Strength: " + this.strength);
+    }
+
+    drinkSake() {
+        this.health += 10;
+        console.log(
+            this.name + " consumed sake. Health is now: " + this.health
+        );
+    }
+}
+
+console.log("\n  Upcoming: Chuck");
+const ninja1 = new ninja("Chuck", 10);
+ninja1.sayName();
+ninja1.showStats();
+ninja1.drinkSake();
+// console.log(ninja1);
+
+console.log("\n  Upcoming: Mary");
+const ninja2 = new ninja("Mary", 15, 5, 7);
+ninja2.sayName();
+ninja2.showStats();
+ninja2.drinkSake();
+// console.log(ninja2);
